01a42fc5e1df2ca3df87e001c14827f9543cf562
Radek Kratochvil <r1_kratochvil@utb.cz>
2024-01-01 18:02:19 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AK5PMRadekZapocet',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
